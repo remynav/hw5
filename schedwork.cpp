ab6ad491960992ae02a6142c05f0110c0997ee34
Remy Navarre <rnavarre@usc.edu>
2025-12-04 01:09:48 +0000

    done

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::vector<size_t>& shiftsUsed,
+    size_t day,
+    size_t slot
+);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -38,8 +47,85 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    // days
+    size_t n = avail.size();
+    //workers
+    size_t k = avail[0].size();
+
+    if(dailyNeed > k){
+      return false;
+    }
+
+    sched.assign(n, std::vector<Worker_T>(dailyNeed, INVALID_ID));
+    std::vector<size_t> shiftsUsed(k, 0);
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsUsed, 0, 0);
 
 
+
+
+
+
+
+}
+
+
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::vector<size_t>& shiftsUsed,
+    size_t day,
+    size_t slot
+)
+{
+  //days
+  size_t n = avail.size();
+  //workers
+  size_t k = avail[0].size();
+
+  //filled all
+  if(day == n){
+    return true;
+  }
+
+  //filled all slots in the curr day
+  if(slot == dailyNeed){
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsUsed, day+1, 0);
+  }
+
+  //try assigning workers
+  for(size_t i = 0; i < k; ++i){
+    
+    //must be avail that day
+    if(!avail[day][i]){
+      continue;
+    }
+    
+    //must not go over max shifts
+    if(shiftsUsed[i] >= maxShifts){
+      continue;
+    }
+    
+    //not alr sched that day
+    if(std::find(sched[day].begin(), sched[day].end(), i) != sched[day].end()){
+      continue;
+    }
+    
+    //choose worker
+    sched[day][slot] = static_cast<Worker_T>(i);
+    shiftsUsed[i]++;
+    
+    //recurse
+    if(scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsUsed, day, slot+1)){
+      return true;
+    }
+    
+    //backtrack
+    shiftsUsed[i]--;
+    sched[day][slot] = INVALID_ID;
+  }
+  return false;
 
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,14 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelper(
+    std::string& curr,
+    const std::string& fixed,
+    string floatingLeft,
+    const std::set<std::string>& dict,
+    set<string>& result,
+    int index,
+    int blanksLeft);
 
 
 // Definition of primary wordle function
@@ -22,7 +30,112 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<std::string> result;
+    string curr = in;
+
+    int blanksLeft = 0;
+    for(int i = 0; i < (int)in.size();i++){
+        if(in[i] == '-'){
+            blanksLeft++;
+        }
+    }
+
+    wordleHelper(curr, in, floating, dict, result, 0, blanksLeft);
+
+    return result;
+
 
 }
 
 // Define any helper functions here
+
+void wordleHelper(
+    std::string& curr,
+    const std::string& fixed,
+    string floatingLeft,
+    const std::set<std::string>& dict,
+    set<string>& result,
+    int index , 
+    int blanksLeft)
+{
+
+    //full word formed
+    if(index == (int)curr.size()){
+        if(floatingLeft.empty() && dict.find(curr) != dict.end()){
+            result.insert(curr);
+        }
+        return;
+    }
+
+        /**
+        //check if floating letters are in the word
+        bool valid = true;
+
+        for(int i=0; i < (int)floating.size(); i++){
+           char c = floating[i];
+           bool inWord = false;
+           for(int j = 0; j < curr.size(); j++){
+                if(curr[j] == c){
+                    inWord = true;
+                    break;
+                }
+           }
+
+            if(!inWord){
+                valid = false;
+                break;
+            }           
+        }
+
+        //check if in dict
+        if(valid && dict.find(curr) != dict.end()){
+            result.insert(curr);
+        }
+        return;
+    }
+    **/
+
+    //if current letter is fixed
+    if(fixed[index] != '-'){
+        curr[index] = fixed[index];
+        wordleHelper(curr, fixed, floatingLeft, dict, result, index+1, blanksLeft);
+        return;
+    }
+    
+    //position is blnak, try letters
+    for(char c = 'a'; c <= 'z'; c++){
+        bool isFloating = false;
+        int floatIdx = -1;
+
+        //check if c is a floating letter
+        for(int i = 0; i < (int)floatingLeft.size(); i++){
+            if(floatingLeft[i] == c){
+                isFloating = true;
+                floatIdx = i;
+                break;
+            }
+        }
+        if(isFloating){
+            //use floating letter in this spot
+            string newFloating = floatingLeft;
+            newFloating.erase(newFloating.begin() + floatIdx);
+
+            curr[index] = c;
+            wordleHelper(curr, fixed, newFloating, dict, result, index+1, blanksLeft-1);
+        }
+        else{
+            //use a non floating letter; make sure remaing spots left for floating letters
+            int remainingSpots = blanksLeft - 1;
+            if(remainingSpots < (int)floatingLeft.size()){
+                //not enough space to fit floating
+                continue;
+            }
+            curr[index] = c;
+            wordleHelper(curr, fixed, floatingLeft, dict, result, index+1, remainingSpots);
+        }
+
+    }
+    curr[index] = '-';
+
+
+}
